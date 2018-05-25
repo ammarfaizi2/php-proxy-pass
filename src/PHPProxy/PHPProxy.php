97c67debf3bce44e6886509c8753167d9a1acd45
Ammar Faizi <ammarfaizi2@gmail.com>
2018-05-26 00:22:07 +0700

    Update

--- src/PHPProxy/PHPProxy.php
+++ src/PHPProxy/PHPProxy.php
@@ -43,6 +43,7 @@ class PHPProxy
 			"request_method" => isset($_SERVER["REQUEST_METHOD"]) ? $_SERVER["REQUEST_METHOD"] : "GET",
 			"headers" => getallheaders()
 		];
+		unset($this->clientRequest["headers"]["Cookie"]);
 		$this->clientRequest["headers"]["Host"] = $this->proxyHost;
 		$this->clientRequest["headers"]["Connection"] = "closed";
 	}
